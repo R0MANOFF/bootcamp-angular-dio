379821fe307baa8934ef7060b673c8309ff9fd04
R0MANOFF <nathasha.pinto@discente.ufma.br>
2023-05-16 13:02:15 -0300

    web components simples feito

---
+++ web-components/src/Components/test.js
@@ -0,0 +1,26 @@
+class CardNews extends HTMLElement{
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({mode: 'open'})
+        
+        //base do componente
+        const componentRoot = document.createElement("h1")
+        componentRoot.textContent = "Nathasha";
+        //estilizacao d componente
+        const style = document.createElement("style");
+        style.textContent = `
+        h1{
+            color:red;
+
+        }
+        `
+        //enviar para a shadow
+        shadow.appendChild(componentRoot)
+        shadow.appendChild(style)
+
+    }
+
+}
+
+customElements.define("titulo-dinamico", CardNews)
